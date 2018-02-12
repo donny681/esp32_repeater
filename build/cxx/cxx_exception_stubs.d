65c255a88c43df41c1afc463c79742294370ce99
Sungki Kim <skkim7@hotmail.com>
2018-02-13 00:55:27 +0900

    remove files

--- build/cxx/cxx_exception_stubs.d
+++
@@ -1,6 +0,0 @@
-cxx_exception_stubs.o: \
- /home/ai-thinker/esp/esp-idf/components/cxx/./cxx_exception_stubs.cpp \
- /home/ai-thinker/esp_demo/esp32_repeater/build/include/sdkconfig.h
-
-/home/ai-thinker/esp_demo/esp32_repeater/build/include/sdkconfig.h:
-/home/ai-thinker/esp/esp-idf/components/cxx/./cxx_exception_stubs.cpp:
--- build/jsmn/src/jsmn.d
+++
@@ -1,5 +0,0 @@
-src//jsmn.o: /home/ai-thinker/esp/esp-idf/components/jsmn/src//jsmn.c \
- /home/ai-thinker/esp/esp-idf/components/jsmn/include/jsmn.h
-
-/home/ai-thinker/esp/esp-idf/components/jsmn/include/jsmn.h:
-/home/ai-thinker/esp/esp-idf/components/jsmn/src//jsmn.c:
